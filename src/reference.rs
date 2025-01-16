6c8cfd21aa52838832851687c763ed331c642ce5
jayasudham-icpl <jayasudham@infopercept.com>
2025-01-16 17:25:59 +0530

    Demonstrate Reference in Rust

--- src/reference.rs
+++ src/reference.rs
@@ -2,5 +2,5 @@ fn main(){
     let s1 = String::from("hello");
     let s2 = &s1;
     println!("{}",s2);
-    println!("{}",s1); // This is valid, The first pointer wasn't invalidated
+    println!("{}",s1); // This is valid because of reference, The first pointer wasn't invalidated
 }
